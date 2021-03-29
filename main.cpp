73c4e353b3e22b804d781cfc39d14638d3383a1e
omarsharaf2 <omar.sharaf@live.ca>
2021-03-29 03:59:18 +0000

    bullet proofing

--- main.cpp
+++ main.cpp
@@ -111,79 +111,133 @@ int main() {
 
   cout<<"enter where you want your x to go: ";
   cin>>commandx;
+  if((arr1[0]!="x")and(arr1[0]!="o"))
+  {
   if (commandx=="up-left")
   {
     arr1[0]="x";
   }
+  }
+  if((arr1[1]!="x")and(arr1[1]!="o"))
+  {
   if (commandx=="up-mid")
   {
     arr1[1]="x";
   }
+  }
+  if((arr1[2]!="x")and(arr1[2]!="o"))
+  {
     if (commandx=="up-right")
   {
     arr1[2]="x";
   }
+  }
+   if((arr1[3]!="x")and(arr1[3]!="o"))
+  {
     if (commandx=="mid-left")
   {
     arr1[3]="x";
   }
+  }
+   if((arr1[4]!="x")and(arr1[4]!="o"))
+  {
     if (commandx=="mid-mid")
   {
     arr1[4]="x";
   }
+  }
+   if((arr1[5]!="x")and(arr1[5]!="o"))
+  {
     if (commandx=="mid-right")
   {
     arr1[5]="x";
   }
+  }
+   if((arr1[6]!="x")and(arr1[6]!="o"))
+  {
    if (commandx=="bottom-left")
   {
     arr1[6]="x";
   }
+  }
+   if((arr1[7]!="x")and(arr1[7]!="o"))
+  {
   if (commandx=="bottom-mid")
   {
     arr1[7]="x";
   }
+  }
+   if((arr1[8]!="x")and(arr1[8]!="o"))
+  {
   if (commandx=="bottom-right")
   {
     arr1[8]="x";
   }
+  }
   cout<<"enter where you want your o to go: ";
   cin>>commando;
+  if((arr1[0]!="x")and(arr1[0]!="o"))
+  {
    if (commando=="up-left")
   {
     arr1[0]="o";
   }
+  }
+  if((arr1[1]!="x")and(arr1[1]!="o"))
+  {
   if (commando=="up-mid")
   {
     arr1[1]="o";
   }
+  }
+  if((arr1[2]!="x")and(arr1[2]!="o"))
+  {
     if (commando=="up-right")
   {
     arr1[2]="o";
   }
+  }
+   if((arr1[3]!="x")and(arr1[3]!="o"))
+  {
     if (commando=="mid-left")
   {
     arr1[3]="o";
   }
+  }
+   if((arr1[4]!="x")and(arr1[4]!="o"))
+  {
     if (commando=="mid-mid")
   {
     arr1[4]="o";
   }
+  }
+   if((arr1[5]!="x")and(arr1[5]!="o"))
+  {
     if (commando=="mid-right")
   {
     arr1[5]="o";
   }
+  }
+  if((arr1[6]!="x")and(arr1[6]!="o"))
+  {
    if (commando=="bottom-left")
   {
     arr1[6]="o";
   }
+  }
+  if((arr1[7]!="x")and(arr1[7]!="o"))
+  {
   if (commando=="bottom-mid")
   {
     arr1[7]="o";
   }
+  }
+  if((arr1[8]!="x")and(arr1[8]!="o"))
+  {
   if (commando=="bottom-right")
   {
     arr1[8]="o";
+  }
   }
   }
   return(0);
